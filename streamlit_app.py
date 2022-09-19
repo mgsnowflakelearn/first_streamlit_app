af501eacc44ca4b7efb89d8021f3c8df34529a4a
Mike Garza <113939456+mgsnowflakelearn@users.noreply.github.com>
2022-09-19 14:16:58 -0500

    Update streamlit_app.py
    matching snowflake tutorial

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.subheader('Breakfast Menu')
-
-streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('Kale, Spinach & Rocket Smoothie')
-streamlit.write('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
